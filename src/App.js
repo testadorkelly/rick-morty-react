d7df0f72816ee67d804dafd6aa2ff400c3387cf0
testadorkelly <kellycrisardaiaardaia@gmail.com>
2021-10-31 21:50:18 -0300

    chamando o componente principal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import HeaderResponsivo from './components/HeaderResponsivo';
+import MainSection from './components/MainSection';
 
 function App() {
   return (
     <div className="App">
       <HeaderResponsivo/>
+      <MainSection/>
     </div>
   );
 }
